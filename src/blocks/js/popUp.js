b65b66e77053e64a64e9aaa6ab91afe31dd3c94a
Vladimir Bulanyi <jetbul@gmail.com>
2020-04-02 00:09:47 +0300

    bugs fix

--- src/blocks/js/popUp.js
+++ src/blocks/js/popUp.js
@@ -15,7 +15,7 @@ export default class Popup {
                 const getPopupImageLink = e.target.attributes.style.value.slice(22, -1);
                 return popuppPicture.insertAdjacentHTML('afterbegin', `<div class="popup popup_is-opened popup-picture">
                 <div class="popup__content place-card__popup" style="background-image: url(${getPopupImageLink})">
-                <img src="src/images/close.svg" alt="" class="popup__close">
+                <img src="./images/close.svg" alt="" class="popup__close">
                 </div>
             </div>`);
             }
